0613706109338d31c7e51da53140ec42f361ad16
muratbayram <bayram.murat@toptal.com>
2021-11-09 00:45:55 +0000

    rest test work, but mongo should be mocked

--- routes/user/user-route.spec.js
+++ routes/user/user-route.spec.js
@@ -7,4 +7,10 @@ describe('users route test',  () => {
     it('should return all users when get called', async () => {
         await request.get('/users').expect(200);
     });
+
+    it('should insert user', async () => {
+        await request.post('/users')
+            .send({username: 'mrt', password: 'byr', email: 'gmail'})
+            .expect(200);
+    });
 })
