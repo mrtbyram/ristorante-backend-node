0e7250622715f11966ff929c88abc32befb3e8f3
muratbayram <bayram.murat@toptal.com>
2021-11-09 12:00:22 +0000

    src folder created

--- src/service/mongo-service.js
+++ src/service/mongo-service.js
@@ -4,7 +4,7 @@ const mockClient = mongoMock.MongoClient;
 mockClient.persist="./node_modules/mongo-mock-db.js";
 
 const mongoClient = new MongoClient('mongodb+srv://byram:byram@cluster0.uymvn.mongodb.net/ristorante?retryWrites=true&w=majority');
-const connect = process.env.NODE_ENV == 'prod'
+const connect = process.env.NODE_ENV == 'production'
     ? mongoClient.connect()
     : mockClient.connect('mongodb://localhost:27017/myproject');
 const db = connect.then(c => c.db('ristorante'));
