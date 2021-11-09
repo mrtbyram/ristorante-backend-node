2ac48dbb4ecc4830f141eea367a90bc646db5fa9
muratbayram <bayram.murat@toptal.com>
2021-11-09 01:18:05 +0000

    mongo mock setup

--- service/mongo-service.js
+++ service/mongo-service.js
@@ -1,6 +1,12 @@
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
+const mongoMock = require('mongo-mock');
+const mockClient = mongoMock.MongoClient;
+mockClient.persist="./node_modules/mongo-mock-db.js";
 
 const mongoClient = new MongoClient('mongodb+srv://byram:byram@cluster0.uymvn.mongodb.net/ristorante?retryWrites=true&w=majority');
-const db = mongoClient.connect().then(c => c.db('ristorante'));
+const connect = process.env.NODE_ENV == 'prod'
+    ? mongoClient.connect()
+    : mockClient.connect('mongodb://localhost:27017/myproject');
+const db = connect.then(c => c.db('ristorante'));
 
 module.exports = db;
