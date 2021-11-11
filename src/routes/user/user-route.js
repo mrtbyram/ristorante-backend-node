469002f09237cbb12596c32b9979a94ba5a37bda
muratbayram <bayram.murat@toptal.com>
2021-11-11 20:56:41 +0000

    only way to satisfy both test and running app

--- src/routes/user/user-route.js
+++ src/routes/user/user-route.js
@@ -5,32 +5,36 @@ const {userNotFound} = require("../../error/ristorante-errors");
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/',  (req, res, next) => {
     const page = parseInt(req.query.page || defaultPage()) - 1;
     const size = parseInt(req.query.size || defaultSize());
 
-    (await db).collection('users').find()
+    db.then(d => d.collection('users').find()
         .sort({name: 1})
         .skip(page * size)
         .limit(size)
-        .toArray((err, items) => res.send(items));
+        .toArray((err, items) => res.send(items)))
+        .catch(next);
 })
 
-router.get('/:username', async (req, res, next) => {
-    (await db).collection('users').findOne({username: req.params.username},
+router.get('/:username', (req, res, next) => {
+    db.then(d => d.collection('users').findOne({username: req.params.username},
         (err, item) => !item
         ? next(userNotFound(req.params.username))
-        : res.send(item))
+        : res.send(item)))
+        .catch(next);
 })
 
-router.post('/', async (req, res, next) => {
-    (await db).collection('users').insertOne(extractUser(req.body),
-        (err, result) => res.status(201).send());
+router.post('/',  (req, res, next) => {
+    db.then(d => d.collection('users').insertOne(extractUser(req.body),
+        (err, result) => res.status(201).send()))
+        .catch(next);
 })
 
-router.delete('/:id', async (req, res) => {
-    (await db).collection('users').deleteOne({id: req.params.id}, {},
-        (err, result) => res.send());
+router.delete('/:id', async (req, res, next) => {
+    db.then(d => d.collection('users').deleteOne({id: req.params.id}, {},
+        (err, result) => res.send()))
+        .catch(next);
 })
 
 module.exports = router;
--- src/service/mongo-service.js
+++ src/service/mongo-service.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require("mongodb");
 const mongoMock = require('mongo-mock');
+const {dbConnectionError} = require("../error/ristorante-errors");
 const mockClient = mongoMock.MongoClient;
 mockClient.persist="./node_modules/mongo-mock-db.js";
 
@@ -7,6 +8,11 @@ const mongoClient = new MongoClient('mongodb+srv://byram:byram@cluster0.uymvn.mo
 const connect = process.env.NODE_ENV == 'production'
     ? mongoClient.connect()
     : mockClient.connect('mongodb://localhost:27017/myproject');
-const db = connect.then(c => c.db('ristorante'));
+const db = connect
+    .then(c => c.db('ristorante'))
+    .catch(err => {
+        console.log(err);
+        return { collection: (c) => { throw dbConnectionError() } }
+    });
 
 module.exports = db;
