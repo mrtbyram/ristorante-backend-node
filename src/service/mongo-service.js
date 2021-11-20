6d9003a1e6b0760741c265affa79523504fc90fe
muratbayram <bayram.murat@toptal.com>
2021-11-20 17:31:29 +0000

    config files have been added

--- src/service/mongo-service.js
+++ src/service/mongo-service.js
@@ -3,11 +3,13 @@ const mongoMock = require('mongo-mock');
 const {dbConnectionError} = require("../error/ristorante-errors");
 const mockClient = mongoMock.MongoClient;
 mockClient.persist="./node_modules/mongo-mock-db.js";
+const config = require('config');
 
-const mongoClient = new MongoClient('mongodb+srv://byram:byram@cluster0.uymvn.mongodb.net/ristorante?retryWrites=true&w=majority');
-const connect = process.env.NODE_ENV == 'production'
-    ? mongoClient.connect()
-    : mockClient.connect('mongodb://localhost:27017/myproject');
+
+let dbUrl = config.get('db.mongo');
+const mongoClient = new MongoClient(dbUrl);
+const connect = process.env.NODE_ENV == 'production' ? mongoClient.connect() : mockClient.connect(dbUrl);
+
 const db = connect
     .then(c => c.db('ristorante'))
     .catch(err => {
